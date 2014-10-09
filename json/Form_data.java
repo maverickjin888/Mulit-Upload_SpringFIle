d22e78043ba6cc519dd3d77c7bd5715568db7ef2
maverickjin888 <maverickjin888@naver.com>
2014-10-09 14:31:37 +0900

    made from Bit_Place

---
+++ json/Form_data.java
@@ -0,0 +1,47 @@
+package bitplace.vo;
+
+import java.util.List;
+
+public class Form_data {
+
+	List<Bean> list;
+	
+	List<Bean> list2;
+
+	List<Bean> list3;
+	
+	List<Bean> list4;
+	
+	
+	public List<Bean> getList4() {
+		return list4;
+	}
+
+	public void setList4(List<Bean> list4) {
+		this.list4 = list4;
+	}
+
+	public List<Bean> getList3() {
+		return list3;
+	}
+
+	public void setList3(List<Bean> list3) {
+		this.list3 = list3;
+	}
+
+	public List<Bean> getList2() {
+		return list2;
+	}
+
+	public void setList2(List<Bean> list2) {
+		this.list2 = list2;
+	}
+
+	public List<Bean> getList() {
+		return list;
+	}
+
+	public void setList(List<Bean> list) {
+		this.list = list;
+	}
+}
